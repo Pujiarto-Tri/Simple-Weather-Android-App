a4a4a1c698eece569bf64a8abee295b8bfc77d9d
pujiarto <pujiarto.ta@gmail.com>
2022-05-09 12:24:10 +0800

    Added menu to choose between F and C in main activity

--- app/src/main/java/com/example/ad340/MainActivity.kt
+++ app/src/main/java/com/example/ad340/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.ad340
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -56,6 +60,38 @@ class MainActivity : AppCompatActivity() {
         forecastDetailsIntent.putExtra("key_temp", forecast.temp)
         forecastDetailsIntent.putExtra("key_description", forecast.description)
         startActivity(forecastDetailsIntent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.tempDisplaySetting -> {
+                showTempDisplaySettingDialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showTempDisplaySettingDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setTitle("Chose Display Unit")
+            .setMessage("Choose which temperature unit to use for temperature display")
+            .setPositiveButton("F°"){ _, _ ->
+                tempDisplaySettingManager.updateSetting(TempDisplaySetting.Fahrenheit)
+            }
+            .setNeutralButton("C°"){_, _ ->
+                tempDisplaySettingManager.updateSetting(TempDisplaySetting.Celsius)
+            }
+            .setOnDismissListener{
+                Toast.makeText(this, "Setting will take affect after app restart", Toast.LENGTH_SHORT).show()
+            }
+        dialogBuilder.show()
     }
 }
 
